78d942ed406a9e97aa3cf0e773ecd4134dbbb182
yuunh <abcdef1470@naver.com>
2023-04-04 14:24:07 +0900

    Scheduled
    Scheduled

---
+++ ajaxProject/src/main/java/com/kh/ajax/controller/Test.java
@@ -0,0 +1,13 @@
+package com.kh.ajax.controller;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Test {
+
+	@Scheduled(cron = "*/1 * * * * ?")
+	public void test1() {
+		System.out.println("스케줄링테스트");
+	}
+}
